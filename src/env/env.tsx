bd8817ae1b0ad27c1c1268f506df82483291f5c0
Samuel Beaulieu <samuel.b1015@hotmail.com>
2022-05-07 17:55:13 -0400

    env config

--- src/env/env.tsx
+++ src/env/env.tsx
@@ -1,7 +1,7 @@
 import { Environment } from "./Environment";
 
 export const env: Environment = {
-    apiEndpoint: "http://10.0.0.104:8000",
+    apiEndpoint: "http://10.0.0.104/api",
     apiVersion: "v1",
     production: false,
 };
